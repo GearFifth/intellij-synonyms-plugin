e862a4d025e2d6125260e6a40756ae3c334eb11c
GitHub Action <action@github.com>
2023-12-12 11:02:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/gearfifth/intellijsynonymsplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/gearfifth/intellijsynonymsplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gearfifth.intellijsynonymsplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
