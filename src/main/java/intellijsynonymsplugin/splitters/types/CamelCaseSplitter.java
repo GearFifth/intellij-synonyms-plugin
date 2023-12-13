18cad51d2cd4eeeebeebf86ef414cc4262a8a231
Ilija Bešlin <44114597+GearFifth@users.noreply.github.com>
2023-12-13 18:41:38 +0100

    feat: implemented CamelCaseSplitter

--- src/main/java/intellijsynonymsplugin/splitters/types/CamelCaseSplitter.java
+++ src/main/java/intellijsynonymsplugin/splitters/types/CamelCaseSplitter.java
@@ -2,11 +2,36 @@ package intellijsynonymsplugin.splitters.types;
 
 import intellijsynonymsplugin.splitters.interfaces.IdentifierSplitter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CamelCaseSplitter implements IdentifierSplitter {
     @Override
     public List<String> splitIdentifier(String identifier) {
-        return null;
+        List<String> result = new ArrayList<>();
+
+        if (identifier == null || identifier.isEmpty()) {
+            return result;
+        }
+
+        StringBuilder currentWord = new StringBuilder();
+        currentWord.append(identifier.charAt(0));
+
+        for (int i = 1; i < identifier.length(); i++) {
+            char currentChar = identifier.charAt(i);
+
+            if (Character.isUpperCase(currentChar)) {
+                result.add(currentWord.toString());
+                currentWord = new StringBuilder();
+            }
+
+            currentWord.append(currentChar);
+        }
+
+        result.add(currentWord.toString());
+
+        return result;
     }
 }
